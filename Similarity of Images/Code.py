84e44e30c3bad373e6e30812c289706ac90924a8
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2018-10-25 20:45:59 -0400

    Modifying code to make it generic for both GSC and Human Observed Data Set

--- Similarity of Images/Code.py
+++ Similarity of Images/Code.py
@@ -12,9 +12,10 @@ import pandas as pd
 
     
     
-def extract_features_gsc_concat(feature_file,same_pairs,targetValue):
-    feature_writer_1=['f'+str(i) for i in range(1, 513)]
-    feature_writer_2=['f'+str(i) for i in range(513, 1025)]
+def extract_features_concat(feature_file,same_pairs,targetValue,feature_length):
+    feature_writer_1=['f'+str(i) for i in range(1, feature_length)]
+    x=feature_length
+    feature_writer_2=['f'+str(i) for i in range(feature_length+1, x*2)]
     features=[]
     for index,row in same_pairs.iterrows():
         if index>10:
@@ -63,10 +64,22 @@ def preprocess_gsc():
     feature_file=read_file("GSC-Features-Data/GSC-Features.csv")
     same_pairs=read_file("GSC-Features-Data/same_pairs.csv")
     diff_pairs=read_file("GSC-Features-Data/diffn_pairs.csv")
-    same_pairs_features_concat=extract_features_gsc_concat(feature_file,same_pairs,1)
-    diff_pairs_features_concat=extract_features_gsc_concat(feature_file,diff_pairs,0)
+    same_pairs_features_concat=extract_features_concat(feature_file,same_pairs,1,513)
+    diff_pairs_features_concat=extract_features_concat(feature_file,diff_pairs,0,513)
     same_pairs_features_sub=np.array(extract_features_gsc_sub(feature_file,same_pairs,1))
     diff_pairs_features_sub=np.array(extract_features_gsc_sub(feature_file,diff_pairs,0))
     return same_pairs_features_concat,diff_pairs_features_concat,same_pairs_features_sub,diff_pairs_features_sub
 
-same_pairs_features_concat,diff_pairs_features_concat,same_pairs_features_sub,diff_pairs_features_sub=preprocess_gsc()
+def preprocess_human_observed():
+    feature_file=read_file("HumanObserved-Features-Data/HumanObserved-Features-Data.csv")
+    feature_file=feature_file.drop(feature_file.columns[0],axis=1)
+    same_pairs=read_file("HumanObserved-Features-Data/same_pairs.csv")
+    diff_pairs=read_file("HumanObserved-Features-Data/diffn_pairs.csv")
+    same_pairs_features_concat=extract_features_concat(feature_file,same_pairs,1,10)
+    diff_pairs_features_concat=extract_features_concat(feature_file,diff_pairs,0,10)
+    same_pairs_features_sub=np.array(extract_features_gsc_sub(feature_file,same_pairs,1))
+    diff_pairs_features_sub=np.array(extract_features_gsc_sub(feature_file,diff_pairs,0))
+    return same_pairs_features_concat,diff_pairs_features_concat,same_pairs_features_sub,diff_pairs_features_sub
+
+gsc_same_pairs_features_concat,gsc_diff_pairs_features_concat,gsc_same_pairs_features_sub,gsc_diff_pairs_features_sub=preprocess_gsc()
+human_same_pairs_features_concat,human_diff_pairs_features_concat,human_same_pairs_features_sub,human_diff_pairs_features_sub=preprocess_human_observed()
