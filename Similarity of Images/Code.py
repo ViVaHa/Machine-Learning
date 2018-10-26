d7134e1d45a50250adf687b5d9ca36e7a3999c17
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2018-10-25 20:08:16 -0400

    Modifying code for feature concatenation and feature subtraction.

--- Similarity of Images/Code.py
+++ Similarity of Images/Code.py
@@ -17,7 +17,7 @@ def extract_features_gsc_concat(same_pairs,targetValue):
     feature_writer_2=['f'+str(i) for i in range(513, 1025)]
     features=[]
     for index,row in same_pairs.iterrows():
-        if index>100:
+        if index>10:
             break
         writer_a=row['img_id_A']
         writer_b=row['img_id_B']
@@ -33,28 +33,26 @@ def extract_features_gsc_concat(same_pairs,targetValue):
         else:
             features=features.append(concatenated_features)
     features['target']=targetValue
-    return features
+    return features.values
 
 
 def extract_features_gsc_sub(same_pairs,targetValue):
     features=[]
     for index,row in same_pairs.iterrows():
-        if index>1:
+        if index>10:
             break
         writer_a=row['img_id_A']
         writer_b=row['img_id_B']
         features_a=feature_file.loc[feature_file['img_id'] == writer_a].drop(['img_id'], axis=1)
         features_b=feature_file.loc[feature_file['img_id'] == writer_b].drop(['img_id'], axis=1)
-        return abs(features_a.values - features_b.values)
-        '''
-        concatenated_features=pd.concat([features_a, features_b],axis=1)
+        sub_features=abs(features_a.values - features_b.values)
         if index<1:
-            features=concatenated_features
+            features=sub_features
         else:
-            features=features.append(concatenated_features)
-    features['target']=targetValue
+            features=np.concatenate((features,sub_features),axis=0)
+    target=[[targetValue for i in range(1)] for j in range(len(features))]
+    features = np.concatenate((features, target), 1)
     return features
-        '''
 
 
 def read_file(fileName):
@@ -64,20 +62,7 @@ def read_file(fileName):
 feature_file=read_file("GSC-Features-Data/GSC-Features.csv")
 same_pairs=read_file("GSC-Features-Data/same_pairs.csv")
 diff_pairs=read_file("GSC-Features-Data/diffn_pairs.csv")
-same_pairs_features=extract_features_gsc_concat(same_pairs,1)
-diff_pairs_features=extract_features_gsc_concat(diff_pairs,0)
-x=extract_features_gsc_sub(diff_pairs,0)
- 
-'''    
-writers_a = features['img_id_A']
-writers_b=features['img_id_B']
-targets=features['target']
-features.drop(labels=['img_id_A','img_id_B','target'], axis=1,inplace = True)
-features.insert(0, 'img_id_B', writers_b)
-features.insert(0, 'img_id_A', writers_a)
-features.insert(2, 'target', targets)
-'''
-
-
-
-
+same_pairs_features_concat=extract_features_gsc_concat(same_pairs,1)
+diff_pairs_features_concat=extract_features_gsc_concat(diff_pairs,0)
+same_pairs_features_sub=np.array(extract_features_gsc_sub(same_pairs,1))
+diff_pairs_features_sub=np.array(extract_features_gsc_sub(diff_pairs,0))
